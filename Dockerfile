ece04dae086727325017dbb7c1b9127be9eb5968
moad <mouaddehbi0@gmail.com>
2024-05-30 16:38:46 +0200

    Add existing project files prior to the push to GitHub.

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM rwthika/acdc-notebooks:latest
---
+++ docker/docker_build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#docker buildx create --use
+
+docker buildx build \
+--platform linux/amd64,linux/arm64 \
+--cache-from type=registry,ref=rwthika/acdc-notebooks:latest \
+--tag rwthika/acdc-notebooks:latest \
+--output type=docker \
+.
